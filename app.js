e364f458dbb28d6fe3284ffeb513f487a45f9549
Quentin Brogly <qbrogly@gmail.com>
2020-08-01 14:00:14 -0700

    All tests pass

--- app.js
+++ app.js
@@ -21,6 +21,9 @@ const height = 600;
 // svg
 const svg = d3.select("#chart").append("svg").attr("width", width).attr("height", height).attr("id", "svg");
 
+// tooltip
+const toolTip = d3.select("body").append("div").attr("id", "tooltip");
+
 // states
 const statesJson = topojson.feature(countiesData, countiesData.objects.states);
 const statesContainer = d3.select("#svg").append("g").attr("id", "states");
@@ -45,7 +48,66 @@ countiesContainer
   .attr("class", "county")
   .attr("fill", (d) => fillColor(d["properties"]["bachelorsOrHigher"]))
   .attr("data-fips", (d) => d["id"])
-  .attr("data-education", (d) => d["properties"]["bachelorsOrHigher"]);
+  .attr("data-education", (d) => d["properties"]["bachelorsOrHigher"])
+  .on("mouseover", (d) => {
+    d3.select("#tooltip")
+      .style("opacity", 0.8)
+      .attr("data-education", d["properties"]["bachelorsOrHigher"])
+      .text(`${d["properties"]["area_name"]}, ${d["properties"]["state"]}: ${d["properties"]["bachelorsOrHigher"]}%`);
+  })
+  .on("mouseout", () => d3.select("#tooltip").style("opacity", 0))
+  .on("mousemove", () =>
+    d3
+      .select("#tooltip")
+      .style("left", d3.event.pageX + 20 + "px")
+      .style("top", d3.event.pageY - 80 + "px")
+  );
+
+// legend
+const legend = d3.select("#svg").append("svg").attr("id", "legend");
+
+const legendData = [
+  {
+    text: "< 20%",
+    color: "#e5f5e0",
+  },
+  {
+    text: "< 40%",
+    color: "#a1d99b",
+  },
+  {
+    text: "< 60%",
+    color: "#41ab5d",
+  },
+  {
+    text: "> 60%",
+    color: "#00441b",
+  },
+];
+
+legend
+  .selectAll("g.legend")
+  .data(legendData)
+  .enter()
+  .append("g")
+  .attr("transform", function (d, i) {
+    return `translate(${i * 100 + 300}, 0)`;
+  })
+  .each(function (d) {
+    d3.select(this)
+      .append("rect")
+      .attr("width", 30)
+      .attr("height", 15)
+      .attr("fill", d.color)
+      .attr("stroke", "rgb(0,0,0)");
+    d3.select(this)
+      .append("text")
+      .attr("text-anchor", "start")
+      .attr("x", 40)
+      .attr("y", 15 / 2)
+      .attr("dy", "0.35em")
+      .text(d.text);
+  });
 
 function addEducationData(county) {
   county.properties = educationData.filter((countyData) => countyData["fips"] == county.id)[0];
