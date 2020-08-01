cd0b0f7ccbff603cd58de180c4317ef736f7139e
Quentin Brogly <qbrogly@gmail.com>
2020-08-01 12:19:10 -0700

    Map both endpoints data

--- app.js
+++ app.js
@@ -22,21 +22,47 @@ const height = 600;
 const svg = d3.select("#chart").append("svg").attr("width", width).attr("height", height).attr("id", "svg");
 
 // states
+const statesJson = topojson.feature(countiesData, countiesData.objects.states);
 const statesContainer = d3.select("#svg").append("g").attr("id", "states");
 statesContainer
   .selectAll("path")
-  .data(topojson.feature(countiesData, countiesData.objects.states).features)
+  .data(statesJson.features)
   .enter()
   .append("path")
   .attr("d", d3.geoPath())
   .attr("fill", "none");
 
 // counties
+const countiesJson = topojson.feature(countiesData, countiesData.objects.counties);
+const finalData = countiesJson.features.map((county) => addEducationData(county));
 const countiesContainer = d3.select("#svg").append("g").attr("id", "counties");
 countiesContainer
   .selectAll("path")
-  .data(topojson.feature(countiesData, countiesData.objects.counties).features)
+  .data(finalData)
   .enter()
   .append("path")
   .attr("d", d3.geoPath())
-  .attr("class", "county");
+  .attr("class", "county")
+  .attr("fill", (d) => fillColor(d["properties"]["bachelorsOrHigher"]))
+  .attr("data-fips", (d) => d["id"])
+  .attr("data-education", (d) => d["properties"]["bachelorsOrHigher"]);
+
+function addEducationData(county) {
+  county.properties = educationData.filter((countyData) => countyData["fips"] == county.id)[0];
+  return county;
+}
+
+function fillColor(score) {
+  if (Number(score) < 20) {
+    return "#e5f5e0";
+  }
+  if (Number(score) < 40) {
+    return "#a1d99b";
+  }
+  if (Number(score) < 60) {
+    return "#41ab5d";
+  }
+  if (Number(score) > 60) {
+    return "#00441b";
+  }
+}
